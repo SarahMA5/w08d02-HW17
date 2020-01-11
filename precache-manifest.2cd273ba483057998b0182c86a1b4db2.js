11aba0d1ea46af693633087c0eaf95f524ef92ad
Sarah <alahmadisr@gmail.com>
2020-01-11 05:40:49 +0300

    Updates

---
+++ precache-manifest.2cd273ba483057998b0182c86a1b4db2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a748078376d5e6fcae8f69345ac5857",
+    "url": "/w08d02-HW17/index.html"
+  },
+  {
+    "revision": "cf4ab07316a996eb7c86",
+    "url": "/w08d02-HW17/static/css/main.282f97e7.chunk.css"
+  },
+  {
+    "revision": "b989dac9dbb7bee4108c",
+    "url": "/w08d02-HW17/static/js/2.ebecf277.chunk.js"
+  },
+  {
+    "revision": "cf4ab07316a996eb7c86",
+    "url": "/w08d02-HW17/static/js/main.e6f76cc9.chunk.js"
+  },
+  {
+    "revision": "0efd6d830de7bec0fd6e",
+    "url": "/w08d02-HW17/static/js/runtime~main.abf36312.js"
+  }
+]);
